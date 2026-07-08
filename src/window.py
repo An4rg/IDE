a0f55befc5e1322ceef2a99b46c487b3296cce9e
Anarg <charles.godefroy@proton.me>
2026-07-08 23:38:22 +0200

    optimized and commented the app and allowed to use the system default mono font.

--- src/window.py
+++ src/window.py
@@ -1,6 +1,12 @@
-from gi.repository import Adw, Gtk, Gdk
+# This code is under GNU GPL 3.0. You can't reuse this code to produce non-free softwares.
+# In GTK4, we separate the app from the window
+# Here you are in the main window logic.
+# You'll find the widget structure in src/window.ui :)
 
-class IDEConfig:
+from gi.repository import Adw, Gtk, Gdk # Gtk4 imports
+
+
+class IDEConfig: # the main object with every config you will find, very practical to make the settings 
     def __init__(self):
         self.CodeEditorFontSize = 25
         self.EditorBackgroundColor = '#1A1A1A'
@@ -9,15 +15,15 @@ config = IDEConfig()
 
 
 @Gtk.Template(resource_path='/dev/anarg/PortableIDE/window.ui')
-class IdeWindow(Adw.ApplicationWindow):
+class IdeWindow(Adw.ApplicationWindow): # The window code
     __gtype_name__ = 'IdeWindow'
 
-    code_editor = Gtk.Template.Child()
+    code_editor = Gtk.Template.Child() # Getting the code widget
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        css = Gtk.CssProvider()
+        css = Gtk.CssProvider() #adding the CSS to the app
         css.load_from_data(f"""
             .CodeEditor {{
                 font-size: {config.CodeEditorFontSize}px;
@@ -29,7 +35,7 @@ class IdeWindow(Adw.ApplicationWindow):
             }}
         """.encode())
         
-
+        # Applying the Css
         Gtk.StyleContext.add_provider_for_display(
             Gdk.Display.get_default(),
             css,
