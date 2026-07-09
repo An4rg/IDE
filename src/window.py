998402c0c846fe8a37d430bd32f83b69a3518bde
Anarg <charles.godefroy@proton.me>
2026-07-09 02:52:05 +0200

    got the pop up showing, but not the commands and the code is not welled written and has a lot of bugs

--- src/window.py
+++ src/window.py
@@ -13,6 +13,41 @@ class IDEConfig: # the main object with every config you will find, very practic
 
 config = IDEConfig()
 
+# the command prompt for debugging and power users
+class CommandPalette(Gtk.Revealer):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.set_transition_type(Gtk.RevealerTransitionType.SLIDE_DOWN)
+        self.set_transition_duration(150)
+        self.set_valign(Gtk.Align.START)
+        self.set_halign(Gtk.Align.CENTER)
+
+        # Conteneur visuel de la palette
+        box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        box.add_css_class("card")  # style libadwaita "carte" flottante
+        box.set_margin_top(12)
+
+        self.entry = Gtk.Entry()
+        self.entry.set_placeholder_text("Tapez une commande…")
+        self.entry.set_width_chars(50)
+        self.entry.connect("activate", self.on_command_entered)
+
+        box.append(self.entry)
+        self.set_child(box)
+        self.set_reveal_child(False)
+
+    def toggle(self):
+        showing = self.get_reveal_child()
+        self.set_reveal_child(not showing)
+        if not showing:
+            self.entry.grab_focus()
+
+    def on_command_entered(self, entry):
+        command = entry.get_text()
+        print(f"Commande exécutée : {command}")
+        self.set_reveal_child(False)
+        entry.set_text("")
+        
 
 @Gtk.Template(resource_path='/dev/anarg/PortableIDE/window.ui')
 class IdeWindow(Adw.ApplicationWindow): # The window code
@@ -22,6 +57,9 @@ class IdeWindow(Adw.ApplicationWindow): # The window code
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+
+        
+
 
         css = Gtk.CssProvider() #adding the CSS to the app
         css.load_from_data(f"""
@@ -41,3 +79,21 @@ class IdeWindow(Adw.ApplicationWindow): # The window code
             css,
             Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION,
         )
+
+        # test = Gtk.Label(label="abab")
+        overlay = Gtk.Overlay()
+        self.set_content(overlay)
+        overlay.set_child(self.code_editor)
+
+        self.command_palette = CommandPalette()
+        overlay.add_overlay(self.command_palette)
+
+        # Raccourci clavier pour ouvrir/fermer
+        controller = Gtk.ShortcutController()
+        controller.add_shortcut(
+            Gtk.Shortcut.new(
+                Gtk.ShortcutTrigger.parse_string("<Ctrl><Shift>p"),
+                Gtk.CallbackAction.new(lambda *a: self.command_palette.toggle())
+            )
+        )
+        self.add_controller(controller)
